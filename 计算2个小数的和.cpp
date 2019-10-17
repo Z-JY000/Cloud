c337777190c41127f10b0136de7d7710720d95ff
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-10-17 19:26:49 +0800

    计算2个小数的和
    不限制输入小数的位数
    可以定义输出结果的小数的位数
    如果输入小数与输出小数的位数不匹配，则最终计算时遵循数学四舍五入原则

---
+++ 计算2个小数的和.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	float a=0,b=0,result=0;
+	printf("enter the first number:");
+	scanf("%f",&a);
+	printf("enter the second number:");
+	scanf("%f",&b);
+	result=a+b;
+	printf("sum 2 numbers:%.1f",result);
+	return 0;
+}
