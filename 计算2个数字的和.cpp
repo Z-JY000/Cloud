3f7032f0c0d737f25e1c5f8644c26907fdeb75f2
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-10-17 10:41:52 +0800

    计算两个整数的和
    1.int 等标识符不能间隔开写否则无法识别
    2.scanf与printf函数里面分号的使用对象并不相同
    3.定义a,b,result为0或者不赋值均可以实现最后结果，但为了规范，建议首先定义其为0

---
+++ 计算2个数字的和.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main()
+{
+	int a,b,result;
+	
+	printf("enter first number:");
+	scanf("%d",&a);
+	printf("enter second number:");
+	scanf("%d",&b);
+	result=a+b;
+	printf("sum of 2 numbers:%d",result);
+	
+	return 0 ;
+	
+	
+}
