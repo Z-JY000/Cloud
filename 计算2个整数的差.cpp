4248ef6b444cd850fce683c07c5de76ed8526967
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-10-17 19:45:55 +0800

    计算2个整数的差
    与近义程序相同，并且可以得到复数，但我遇到些问题需要解决。

---
+++ 计算2个整数的差.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a=0,b=0,result=0;
+	printf("enter the first number:");
+	scanf("%d",&a);
+	printf("enter the second number:");
+	scanf("%d",&b);
+	result=a-b;
+	printf("sub of 2 numbers:%d",result);
+	return 0;
+	
+}
