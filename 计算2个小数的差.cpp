f8e319bbed14e2134f7cf7566ebb19b85b102aec
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-10-17 20:05:07 +0800

    计算2个小数的差
    如果不定义输出小数的位数，那么默认将输出至小数点后6位

---
+++ 计算2个小数的差.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	float a,b,result;
+	printf("enter the first number:");
+	scanf("%f",&a);
+	printf("enter the second number:");
+	scanf("%f",&b);
+	result=a-b;
+	printf("sub of 2 numbers:%f",result);
+	return 0; 
+ } 
