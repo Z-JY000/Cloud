0286a7328b80c26018c48314da81e127f1d14395
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-11-06 20:44:50 +0800

    比较两个整数的大小

---
+++ 比较两数大小.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the first number:");
+	scanf("%d",&a);
+	printf("enter the secend number:");
+	scanf("%d",&b);
+	if (a>b)
+	printf("a大于b");
+	else 
+	printf("a不大于b");
+	return 0;
+	
+ } 
