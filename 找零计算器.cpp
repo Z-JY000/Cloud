5953d29589ac64ccc2632c71fe60206c94234a8e
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-11-06 21:12:47 +0800

    简易找零计算器

---
+++ 找零计算器.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int price,bill;
+	printf("请输入金额：");
+	scanf("%d",&price);
+	printf("请输入票面：");
+	scanf("%d",&bill);
+	if (bill>=price)
+	{
+		int change;
+		change=bill-price;
+		printf("找您%d元",change); 
+	 } 
+	 else
+	 {
+	 	int money;
+	 	money=price-bill;
+	 	printf("对不起，您的钱不够,还差%d元",money); 
+	 }
+	 return 0;
+}
