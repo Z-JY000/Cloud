69d3d7d23990016512121b07b55b23ccec74b1b8
Z-JY000 <55837652+Z-JY000@users.noreply.github.com>
2019-12-06 11:03:18 +0800

    求字符串的长度

---
+++ strlen.c
@@ -0,0 +1,11 @@
+//求字符串的长度 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+	char number[100]="I LOVE C";
+	printf("the length is %d\n",strlen(number));
+	system ("pause");
+	return 0;
+}
